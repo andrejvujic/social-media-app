9ea84744d65382e511de8c40928e45442b8f93f6
Andrej Vujic <vujicandrej365@gmail.com>
2021-01-20 18:01:11 +0100

    added profile and activities screen

---
+++ lib/screens/home/activities.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Activities extends StatefulWidget {
+  @override
+  _ActivitiesState createState() => _ActivitiesState();
+}
+
+class _ActivitiesState extends State<Activities> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Activities',
+        ),
+      ),
+    );
+  }
+}
